2fed12c15505d8eb5352e62cd9718ac4b29d8460
ashraful-rahat <spinball61@gamil.com>
2025-04-15 20:31:10 +0600

    user-crud

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -20,6 +20,85 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
+const allUser = async (req: Request, res: Response) => {
+  try {
+    const result = await userServices.allUser();
+
+    res.status(200).json({
+      status: "success",
+      message: "User fetch Successfully",
+      data: result,
+    });
+  } catch (error: any) {
+    console.log(error);
+    res.status(500).json({
+      status: "fail",
+      message: error.message || "Something went wrong",
+    });
+  }
+};
+
+const getSingleUser = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const result = await userServices.getSingleUser(id);
+
+    res.status(200).json({
+      status: "success",
+      message: " Single User fetch  Successfully",
+      data: result,
+    });
+  } catch (error: any) {
+    console.log(error);
+    res.status(500).json({
+      status: "fail",
+      message: error.message || "Something went wrong",
+    });
+  }
+};
+
+const updateUser = async (req: Request, res: Response) => {
+  try {
+    const userData = req.body;
+    const id = req.params.id;
+    await userServices.updateUser(id, userData);
+
+    res.status(200).json({
+      status: "success",
+      message: "User update Successfully",
+    });
+  } catch (error: any) {
+    console.log(error);
+    res.status(500).json({
+      status: "fail",
+      message: error.message || "Something went wrong",
+    });
+  }
+};
+
+const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const result = await userServices.deleteUser(id);
+
+    res.status(200).json({
+      status: "success",
+      message: "User Deleted Successfully",
+      data: result,
+    });
+  } catch (error: any) {
+    console.log(error);
+    res.status(500).json({
+      status: "fail",
+      message: error.message || "Something went wrong",
+    });
+  }
+};
+
 export const userController = {
   createUser,
+  allUser,
+  getSingleUser,
+  updateUser,
+  deleteUser,
 };
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -6,4 +6,12 @@ const router = express.Router();
 
 router.post("/create-user", userController.createUser);
 
+router.get("/", userController.allUser);
+
+router.get("/:id", userController.getSingleUser);
+
+router.patch("/:id", userController.updateUser);
+
+router.delete("/:id", userController.deleteUser);
+
 export const userRoutes = router;
